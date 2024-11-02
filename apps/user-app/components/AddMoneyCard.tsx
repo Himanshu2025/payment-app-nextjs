6d94ce3e69de865c020baaaca78a5878dbbf5e1f
Himanshu2025 <hkulkarni927@gmail.com>
2024-11-02 13:44:23 +1100

    created user-app ui and onRamptransaction logic

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -1,12 +1,11 @@
 "use client"
-import { Button } from "@repo/ui/button";
-import { Card } from "@repo/ui/card";
-import { Center } from "@repo/ui/center";
-import { Select } from "@repo/ui/select";
-import { useState } from "react";
-import { TextInput } from "@repo/ui/textinput";
-import { createOnRampTransaction } from "../app/lib/actions/createOnRamptxn";
-
+import { Button } from "@repo/ui/button"
+import { Card } from "@repo/ui/card"
+import {Center} from "@repo/ui/center"
+import { Select } from "@repo/ui/select"
+import { useState } from "react"
+import { TextInput } from "@repo/ui/textinput"
+import { createOnRampTransaction } from "../app/lib/actions/createOnRamptxn"
 
 const SUPPORTED_BANKS = [{
     name: "Commonwealth Bank", 
@@ -18,13 +17,13 @@ const SUPPORTED_BANKS = [{
 }]; 
 // add money function 
 export const AddMoney = () => {
-    const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
-    const [amount, setAmount] = useState(0);
-    const [provider, setProvider] = useState(SUPPORTED_BANKS[0]?.name || "") 
+    const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl); 
+    const [provider, setProvider] = useState(SUPPORTED_BANKS[0]?.name || "");
+    const  [amount, setAmount] = useState(0); 
     return <Card title="Add Money">
         <div className="w-full">
             <TextInput label={"Amount"} placeholder={"Amount"} onChange={(value) => {
-                setAmount(Number(value))
+                    setAmount(Number(value))
             }}/>
             <div className="py-4 text-left">
                 Bank
@@ -37,7 +36,7 @@ export const AddMoney = () => {
                 value: x.name
             }))} />
             <div className="flex justify-center pt-4">
-                <Button onClick={ async () => {
+                <Button onClick={async () => {
                     await createOnRampTransaction(amount * 100, provider)
                     window.location.href = redirectUrl || ""; 
                 }}>
